3812d424c3486cf67af5933ba135492508280df9
emmawh30 <emma.white30@gmail.com>
2017-01-12 11:28:40 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// create secret number
+var secretNumber = 7;
+
+//ask user for a guess
+
+var guess = prompt ("Guess a number");
+
+//check if guess is correct
+
+if (Number(guess) === secretNumber) {
+	alert("YOU GOT IT RIGHT!");
+}
+
+//otherwise you got it wrong
+
+
+else { 
+	alert ("You got it wrong");
+}
+
+
+
+
+
+
